999315c2001b5b410d48c56b420770b983ca7a26
Timskiy <timweb@bk.ru>
2024-02-12 21:27:20 +0300

    edited

--- app/src/main/java/com/example/langnavigation/MyApplication.java
+++ app/src/main/java/com/example/langnavigation/MyApplication.java
@@ -2,9 +2,7 @@ package com.example.langnavigation;
 
 import android.app.Application;
 import android.content.Context;
-import android.util.Log;
 
-import com.google.android.gms.ads.MobileAds;
 import com.google.android.play.core.splitcompat.SplitCompat;
 
 public class MyApplication extends Application {
@@ -15,14 +13,6 @@ public class MyApplication extends Application {
         Context context = LanguageHelper.getLanguageConfigurationContext(base);
         super.attachBaseContext(context);
         SplitCompat.install(this);
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        MobileAds.initialize(this, initializationStatus -> {
-            Log.d("LANG_TEST", "MobileAds SDK Initialized");
-        });
     }
 }
 
